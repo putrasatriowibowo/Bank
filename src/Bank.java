e3acd6f8fbcd61d7442434a869ed2c38c270c5ec
Putra <putrasatriowibowo294@gmail.com>
2025-11-18 22:57:59 +0700

    sudah selesai semua

--- src/Bank.java
+++ src/Bank.java
@@ -6,10 +6,30 @@ public class Bank {
     static ArrayList<BankAccount> akun = new ArrayList<>();
 
     public static void main(String[] args) {
+        loop:
         while (true){
+            menu();
+            int pilihanMenu = input.nextInt();
+            input.nextLine();
 
-        menu();
+            switch(pilihanMenu){
+                case 1: //BUAT AKUN
+                    buatAkun();
+                    break;
+                case 2: //DEPOSIT
+                    depositAkun();
+                    break;
+                case 3: //WITHDRAW
+                    withdrawAkun();
+                    break;
+                case 4:
+                    break loop;
+                default:
+                    System.out.println("Pilihan tidak valid");
+            }
         }
+
+        System.out.println("\nTerima kasih. Sampai jumpa");
     }
 
     static void menu(){
@@ -22,57 +42,57 @@ public class Bank {
                         4. Keluar
                         """
         );
-        System.out.print("Pilih menu: ");
-        int pilihanMenu = input.nextInt();
-        input.nextLine();
+        System.out.print("\nPilih menu: ");
+    }
 
-        switch(pilihanMenu){
-            case 1: //BUAT AKUN
-                System.out.print("\nMasukan nama pengguna: ");
-                String namaPengguna = input.nextLine();
-
-                akun.add(new BankAccount(namaPengguna));
-                System.out.println("Halo " + akun.get(akun.size()-1).getNamaPengguna());
-                break;
-            case 2: //DEPOSIT
-                if (tampilkanAkun()) break;
-                System.out.print("\nPilih index akun untuk deposit: ");
-                int pilihanAkun = input.nextInt();
-
-                if (pilihanAkun >= akun.size() || pilihanAkun < 0){
-                    System.out.println("Akun tidak tersedia");
-                } else {
-                    System.out.print("Masukan jumlah uang yang ingin di deposit: ");
-                    int uang = input.nextInt();
-                    akun.get(pilihanAkun).deposit(uang);
-                }
-                break;
-            case 3: //WITHDRAW
-                if (tampilkanAkun()) break;
-                System.out.println("\nPilih index akun untuk withdraw: ");
-                pilihanAkun = input.nextInt();
-
-                if (pilihanAkun >= akun.size() || pilihanAkun < 0){
-                    System.out.println("Akun tidak tersedia");
-                } else {
-                    System.out.print("Masukan jumlah uang yang ingin di withdraw: ");
-                    int uang = input.nextInt();
-                    akun.get(pilihanAkun).withdraw(uang);
-                }
-                break;
+    static void tampilkanAkun(){
+        for (int i = 0;i<akun.size();i++){
+            System.out.println("\nAkun index ke- " + i);
+            System.out.println("Nama: " + akun.get(i).getNamaPengguna());
         }
     }
 
-    static boolean tampilkanAkun(){
-        if (akun.isEmpty()){
-            System.out.println("\nTidak ada akun");
-            return true;
+    static void buatAkun(){
+        System.out.print("\nMasukan nama pengguna: ");
+        String namaPengguna = input.nextLine();
+
+        akun.add(new BankAccount(namaPengguna));
+        System.out.println("\nSelamat datang " + akun.get(akun.size()-1).getNamaPengguna());
+    }
+
+    static void depositAkun(){
+        if(akun.isEmpty()){
+            System.out.println("\nBelum ada akun terdaftar");
         } else{
-            for (int i = 0;i<akun.size();i++){
-                System.out.println("\nAkun index ke- " + i);
-                System.out.println("Nama: " + akun.get(i).getNamaPengguna());
+            tampilkanAkun();
+            System.out.print("\nPilih index akun untuk deposit: ");
+            int pilihanAkun = input.nextInt();
+
+            if (pilihanAkun >= akun.size() || pilihanAkun < 0){
+                System.out.println("Akun tidak tersedia");
+            } else {
+                System.out.print("Masukan jumlah uang yang ingin di deposit: ");
+                int uang = input.nextInt();
+                akun.get(pilihanAkun).deposit(uang);
             }
-            return false;
+        }
+    }
+
+    static void withdrawAkun(){
+        if(akun.isEmpty()){
+            System.out.println("\nBelum ada akun terdaftar");
+        } else{
+            tampilkanAkun();
+            System.out.print("\nPilih index akun untuk withdraw: ");
+            int pilihanAkun = input.nextInt();
+
+            if (pilihanAkun >= akun.size() || pilihanAkun < 0){
+                    System.out.println("Akun tidak tersedia");
+            } else {
+                System.out.print("Masukan jumlah uang yang ingin di withdraw: ");
+                int uang = input.nextInt();
+                akun.get(pilihanAkun).withdraw(uang);
+            }
         }
     }
 }
